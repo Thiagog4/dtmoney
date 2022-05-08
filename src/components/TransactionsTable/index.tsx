40cebe510ea2d76439719b4493300ae1aab51e07
Thiagog4 <thiagojoseguedes@hotmail.com>
2022-05-08 17:13:15 -0300

    Confirmado

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,38 +1,32 @@
 import { Container } from "./styles";
 
-export function TransactionsTable(){
-    return(
-            <Container>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Titulo</th>
-                            <th>Valor</th>
-                            <th>Categoria</th>
-                            <th>Data</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>R$12.000</td>
-                            <td>Desenvolvimento</td>
-                            <td>08/05/2022</td>
-                        </tr>
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>R$12.000</td>
-                            <td>Desenvolvimento</td>
-                            <td>08/05/2022</td>
-                        </tr>
-                        <tr>
-                            <td>Desenvolvimento de site</td>
-                            <td>R$12.000</td>
-                            <td>Desenvolvimento</td>
-                            <td>08/05/2022</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </Container>
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de site</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>08/05/2022</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">1.000</td>
+                        <td>Casa</td>
+                        <td>08/05/2022</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
     );
 }
--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -23,6 +23,19 @@ table{
         padding: 1rem 2rem;
         border: 0;
         background: var(--shape);
+        color:var(--text-doby);
+        border-radius: 0.25rem;
+
+        & :first-child{
+            color:var(--text-title);
+        }
+        &.deposit{
+            color:var(--green);
+        }
+
+        &.withdraw{
+            color: var(--red);
+        }
     }
 }
 `;
